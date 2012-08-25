c05e5539acb88712ee349f664c0a0a4b14c760ed
Alexandra Taylor <ataylor@book.com>
2012-08-25 13:12:17 -0700

    adding participant table script

---
+++ database/tables/participant.sql
@@ -0,0 +1,10 @@
+drop table trickleup.participant;
+
+CREATE TABLE trickleup.participant (
+  business_number int(11) NOT NULL,
+  participant_name varchar(100) NOT NULL,
+  staff_name varchar(100) NOT NULL,
+  shg_name varchar(100) NOT NULL,
+  PRIMARY KEY (`business_number`),
+  UNIQUE KEY `business_number` (`business_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='stores basic information for participants';
