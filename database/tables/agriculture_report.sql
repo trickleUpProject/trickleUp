7a756c7d70a5c08feddd92376171ec979c8508a9
Arthur Goldstein <arthur.goldstein@gmail.com>
2012-08-25 17:00:02 -0400

    Initial database scripts for the agriculture tables

---
+++ database/tables/agriculture_report.sql
@@ -0,0 +1,24 @@
+drop table trickleup.agriculture_report;
+
+create table trickleup.agriculture_report (
+ agriculture_report_id int not null auto_increment,
+ business_number       int(11) not null,        #logical key
+ quarter               int,        #logical key
+ month                 int,        #logical key
+ year                  int,        #logical key
+ participant_name      varchar(100) not null,        #logical key
+ shg_name              varchar(100),
+ staff                 varchar(50),
+ crop                  varchar(50), #logical key
+ agr_inf_type          varchar(50),
+ agr_inf_cost          int,
+ std_prod_land         int,
+ age                   varchar(50),
+ input_cost            int,
+ total_prod            int,
+ quantity_sold         int,
+ amount_from_sale      int,
+ value_produced        int,
+ primary key (agriculture_report_id),
+ unique key (business_number, quarter, month, year, participant_name)
+);
